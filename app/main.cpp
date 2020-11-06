d1c964f9cf4776ff39f2a7a3da2232b660819f32
olanser <57589024+olanser@users.noreply.github.com>
2020-11-06 12:58:47 +0200

    add new structure project, add taglib, fix hover playmusic

---
+++ app/main.cpp
@@ -0,0 +1,15 @@
+#include "generalwindow.h"
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QFile file("./app/stylesheet.qss");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(file.readAll());
+    generalWindow w;
+    w.show();
+
+    return a.exec();
+}
